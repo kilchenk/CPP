0d7790c26ce6f68e91df4c90d71beea20d455404
kilchenk <kirill220420054004@gmail.com>
2024-05-02 16:18:02 +0200

    start cpp05, need learn some theory

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kilchenk <kilchenk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 13:47:47 by kilchenk          #+#    #+#             */
+/*   Updated: 2024/05/02 13:48:57 by kilchenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef BUREAUCRAT_HPP
+#   define BUREAUCRAT_HPP
+# include <iostream>
+# include <string>
+# define RED			"\033[0;31m"
+# define GREEN			"\033[0;32m"
+# define RESET_COLOR 	"\033[0m"
+# define RESET_LINE 	"\033[0m" << std::endl
+# define RE_TERMINAL	std::cout << "\e[1;1H\e[2J"
+
+
+#endif
